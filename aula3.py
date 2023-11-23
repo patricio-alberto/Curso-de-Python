561ea4576d2a7781cfbacf69897a93d62b48d8fc
Patricio Alberto Costa de Sousa <95306887+patricio-alberto@users.noreply.github.com>
2023-11-23 19:59:51 -0300

    aula3

---
+++ aula3.py
@@ -0,0 +1,9 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica e Forte
+"""
+
+print("Mamão é melhor que \"coxinha\"") #Escape
+print(r"Mamão é melhor que \"coxinha\"") # r    
+
+# Todos esses exemplos são opcionais.
