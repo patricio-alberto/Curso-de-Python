ff86ad4ff5a030d736d6b7aba3cb1e31d80dbd99
Patricio Alberto Costa de Sousa <95306887+patricio-alberto@users.noreply.github.com>
2023-11-24 18:45:52 -0300

    aula7

---
+++ aula7.py
@@ -0,0 +1,18 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicar variáveis com letras minúsculas.
+
+nome_completo = 'Patrício Alberto Costa de Sousa'
+soma = 2 + 2
+inteiro_um = int('1')
+inteiro_dois = float('1')
+
+print(inteiro_um, type(inteiro_um))
+print(inteiro_dois, type(inteiro_dois))
+
+
+nome = 'Patrício'
+idade = 26
+maior_idade = idade >= 18
+print(nome)
+print(idade)
+print(maior_idade)
