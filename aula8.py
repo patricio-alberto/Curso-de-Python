e764c702bafa60076da5910b8b3f9fa7de7e8668
Patricio Alberto Costa de Sousa <95306887+patricio-alberto@users.noreply.github.com>
2023-11-26 13:42:01 -0300

    aula8

---
+++ aula8.py
@@ -0,0 +1,16 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicar variáveis com letras minúsculas.
+
+nome = 'Patrício'
+sobrenome = 'Alberto Costa de Sousa'
+idade = 26
+ano_nascimento = 2023 - idade
+maior_idade = idade >= 18
+altura_metros = 1.70
+
+print('Nome: {}'.format(nome))
+print('Sobrenome: {}'.format(sobrenome))
+print('Idade: {}'.format(idade))
+print('Ano de Nascimento: {}'.format(ano_nascimento))
+print('É maior de idade: {}'.format(maior_idade))
+print('Altura em metros: {}'.format(altura_metros))
