2750018e400a9cabe29e9130d90275ec2ed2dcff
Patricio Alberto Costa de Sousa <95306887+patricio-alberto@users.noreply.github.com>
2023-11-27 22:02:58 -0300

    aula9

---
+++ aula9.py
@@ -0,0 +1,23 @@
+adicao = 46 + 57
+print(f'Total da Adição: {adicao}')
+
+subtracao = 57 - 46
+print(f'Total da Subtração: {subtracao}')
+
+multiplicacao = 6 * 40
+print(f'Total da Multiplição: {multiplicacao}')
+
+exponenciacao = 4 ** 5
+print(f'Total da Exponencição: {exponenciacao}')
+
+divisao = 100 / 5
+print(f'Total da divisão: {divisao}')
+
+divisao_inteira = 35 // 5
+print(f'Divisão Inteira: {divisao_inteira}')
+
+resto_da_divisao = 40 % 6
+print(f'Resto da Divisão: {resto_da_divisao}')
+
+
+
